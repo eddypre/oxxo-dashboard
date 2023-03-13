60232e8eb7795f7ea8e32c3a0f7cd6fc48a769b4
Eduardo Triana <eddypre@hotmail.com>
2023-03-13 16:01:11 -0600

    Initial commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from myapp.views import hello
+from django.conf.urls.static import static
 from . import views
+from django.conf import settings
 
 urlpatterns = [
     #path('', views.users)
     path('', views.BASE, name='BASE'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_TEMPLATES, document_root=settings.TEMPLATES_ROOT)
