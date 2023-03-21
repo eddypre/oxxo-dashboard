0ef5f90025518967c8d51597738e44f9af398a11
Eduardo Triana <eddypre@hotmail.com>
2023-03-21 11:09:16 -0600

    New filters for views & links fix

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from myapp.views import hello
-from myapp.views import users
+#from myapp.views import hello
+#from myapp.views import users
 
 
 urlpatterns = [
--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from myapp.views import hello
+#from myapp.views import hello
 from django.conf.urls.static import static
 from . import views
 from django.conf import settings
@@ -8,6 +8,10 @@ from django.conf import settings
 urlpatterns = [
     #path('', views.users)
     path('', views.BASE, name='BASE'),
+    path('APLICACIONES', views.APLICACIONES, name='APLICACIONES'),
+    path('INTERNET', views.INTERNET, name='INTERNET'),
+    path('SEGURIDAD', views.SEGURIDAD, name='SEGURIDAD'),
+    path('RED', views.RED, name='RED'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
