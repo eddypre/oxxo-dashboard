1b7974d90bb71da83056db292bb1af3fd8684189
Eduardo Triana <eduardotriana@MacBook-Pro-de-Eduardo.local>
2023-06-13 15:39:48 -0600

    Reinicio de App Service en Azure

--- myapp/urls.py
+++ myapp/urls.py
@@ -34,4 +34,4 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.STATIC_TEMPLATES, document_root=settings.TEMPLATES_ROOT)
+urlpatterns += static(settings.STATIC_TEMPLATES, document_root=settings.TEMPLATES_ROOT)#reinicio de azure
